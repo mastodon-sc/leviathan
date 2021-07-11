824007ad525eeee2afceba69132cb5420bb9651b
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2021-07-11 23:30:00 +0200

    Clean examples.

--- src/test/java/org/mastodon/leviathan/ImportMaskExample.java
+++ src/test/java/org/mastodon/leviathan/ImportMaskExample.java
@@ -1,8 +1,5 @@
 package org.mastodon.leviathan;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.mastodon.leviathan.app.LeviathanAppModel;
 import org.mastodon.leviathan.app.LeviathanKeyConfigContexts;
 import org.mastodon.leviathan.mask.MaskImporter;
@@ -19,30 +16,9 @@ import org.scijava.ui.behaviour.util.Actions;
 import bdv.spimdata.SpimDataMinimal;
 import bdv.spimdata.XmlIoSpimDataMinimal;
 import bdv.viewer.ViewerOptions;
-import ij.ImageJ;
 import mpicbg.spim.data.SpimDataException;
-import net.imglib2.Cursor;
-import net.imglib2.Interval;
-import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.algorithm.labeling.ConnectedComponents;
-import net.imglib2.algorithm.labeling.ConnectedComponents.StructuringElement;
-import net.imglib2.algorithm.morphology.Dilation;
-import net.imglib2.algorithm.morphology.StructuringElements;
-import net.imglib2.converter.Converters;
-import net.imglib2.img.Img;
-import net.imglib2.img.ImgFactory;
-import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.img.display.imagej.ImageJFunctions;
-import net.imglib2.roi.boundary.Boundary;
-import net.imglib2.roi.labeling.ImgLabeling;
-import net.imglib2.roi.labeling.LabelRegion;
-import net.imglib2.roi.labeling.LabelRegions;
-import net.imglib2.type.NativeType;
-import net.imglib2.type.logic.NativeBoolType;
-import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.RealType;
-import net.imglib2.util.Util;
 import net.imglib2.view.Views;
 
 public class ImportMaskExample
@@ -87,64 +63,5 @@ public class ImportMaskExample
 						globalAppActions );
 
 		new LeviathanViewBdv( appModel );
-	}
-
-	private static < T extends IntegerType< T > & NativeType< T > > void process2( final SharedBigDataViewerData sharedBdvData, final JunctionModel model )
-	{
-		ImageJ.main( null );
-
-		@SuppressWarnings( "unchecked" )
-		final RandomAccessibleInterval< T > img = Views.dropSingletonDimensions(
-				( RandomAccessibleInterval< T > ) sharedBdvData.getSources().get( 0 ).getSpimSource().getSource( 0, 0 ) );
-		int m = Integer.MIN_VALUE;
-		for ( final T t : Views.iterable( img ) )
-			m = Math.max( ( int ) t.getRealDouble(), m );
-		final int max = m;
-
-		// Invert so that inside is white.
-		final T type = Util.getTypeFromInterval( img ).createVariable();
-		final RandomAccessibleInterval< T > input = Converters.convertRAI( img, ( i, o ) -> o.setReal( max - ( int ) i.getRealDouble() ), type );
-
-		final ImgFactory< T > factory = Util.getArrayOrCellImgFactory( ( Interval ) input, type );
-		final Img< T > output = factory.create( input );
-
-		// Connected components.
-		final StructuringElement se = StructuringElement.FOUR_CONNECTED;;
-		ConnectedComponents.labelAllConnectedComponents( Views.extendZero( input ), output, se );
-
-		// Label image.
-		m = Integer.MIN_VALUE;
-		for ( final T t : Views.iterable( output ) )
-			m = Math.max( ( int ) t.getRealDouble(), m );
-		final List< Integer > labels = new ArrayList< >( m );
-		for ( int j = 0; j < m; j++ )
-			labels.add( Integer.valueOf( j ) );
-		final ImgLabeling< Integer, T > labeling = ImgLabeling.fromImageAndLabels( output, labels );
-
-		// Boundaries.
-		final Img< NativeBoolType > dilated = ArrayImgs.booleans( img.dimension( 0 ), img.dimension( 1 ) );
-		final Img< T > boundaries = factory.create( img );
-		final RandomAccess< T > ra = boundaries.randomAccess( boundaries );
-		final LabelRegions< Integer > regions = new LabelRegions<>( labeling );
-		for ( final LabelRegion< Integer > region : regions )
-		{
-			final NativeBoolType nbt = new NativeBoolType( false );
-			final RandomAccessibleInterval< NativeBoolType > regionNBT = Converters.convertRAI( region, ( i, o ) -> o.set( i.get() ), nbt );
-			Dilation.dilate( regionNBT, dilated, StructuringElements.square( 1, 2 ), 1 );
-			ImageJFunctions.show( dilated, "" + region.getLabel() );
-
-			final Boundary< NativeBoolType > boundary = new Boundary<>( regionNBT );
-			final Cursor< Void > cursor = boundary.localizingCursor();
-			while ( cursor.hasNext() )
-			{
-				cursor.fwd();
-				ra.setPosition( cursor );
-				ra.get().setInteger( region.getLabel().intValue() );
-			}
-
-			break;
-		}
-
-		ImageJFunctions.show( boundaries, "boundary" );
 	}
 }
