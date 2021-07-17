62f315cda8dcca75c4a3f4fc0cc26ad8a7c6b039
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2021-07-17 14:35:59 +0200

    Rework examples.

---
+++ src/test/java/org/mastodon/leviathan/TestUtils.java
@@ -0,0 +1,135 @@
+package org.mastodon.leviathan;
+
+import org.mastodon.collection.RefCollections;
+import org.mastodon.collection.RefList;
+import org.mastodon.leviathan.algorithms.FindFaces;
+import org.mastodon.leviathan.app.LeviathanMainWindow;
+import org.mastodon.leviathan.app.LeviathanWM;
+import org.mastodon.leviathan.model.cell.CellModel;
+import org.mastodon.leviathan.model.junction.Junction;
+import org.mastodon.leviathan.model.junction.JunctionGraph;
+import org.mastodon.leviathan.model.junction.JunctionModel;
+import org.mastodon.leviathan.model.junction.MembranePart;
+import org.scijava.Context;
+
+import mpicbg.spim.data.SpimDataException;
+
+public class TestUtils
+{
+	public static class GraphBundle
+	{
+
+		public final CellModel cellModel;
+
+		public final JunctionModel junctionModel;
+
+		public final RefList< Junction > junctions;
+
+		public final RefList< MembranePart > membranes;
+
+		public GraphBundle( final CellModel cellModel, final JunctionModel junctionModel, final RefList< Junction > junctions, final RefList< MembranePart > membranes )
+		{
+			this.cellModel = cellModel;
+			this.junctionModel = junctionModel;
+			this.junctions = junctions;
+			this.membranes = membranes;
+		}
+	}
+
+	public static GraphBundle getExampleGraph()
+	{
+
+		// Create example graph. 3 touching hexs.
+		final JunctionModel junctionModel = new JunctionModel( "pixel", "frame" );
+		final JunctionGraph junctionGraph = junctionModel.getGraph();
+		final RefList< Junction > junctions = RefCollections.createRefList( junctionGraph.vertices() );
+		final RefList< MembranePart > membranes = RefCollections.createRefList( junctionGraph.edges() );
+
+		final Junction A = junctionGraph.addVertex().init( 0, new double[] { 10, 0 } );
+		final Junction B = junctionGraph.addVertex().init( 0, new double[] { 20, 10 } );
+		final Junction C = junctionGraph.addVertex().init( 0, new double[] { 20, 20 } );
+		final Junction D = junctionGraph.addVertex().init( 0, new double[] { 10, 30 } );
+		final Junction E = junctionGraph.addVertex().init( 0, new double[] { 0, 20 } );
+		final Junction F = junctionGraph.addVertex().init( 0, new double[] { 0, 10 } );
+		junctions.add( A );
+		junctions.add( B );
+		junctions.add( C );
+		junctions.add( D );
+		junctions.add( E );
+		junctions.add( F );
+
+		final MembranePart ab = junctionGraph.addEdge( A, B ).init();
+		final MembranePart bc = junctionGraph.addEdge( B, C ).init();
+		final MembranePart cd = junctionGraph.addEdge( C, D ).init();
+		final MembranePart de = junctionGraph.addEdge( D, E ).init();
+		final MembranePart ef = junctionGraph.addEdge( E, F ).init();
+		final MembranePart fa = junctionGraph.addEdge( F, A ).init();
+		membranes.add( ab );
+		membranes.add( bc );
+		membranes.add( cd );
+		membranes.add( ab );
+		membranes.add( de );
+		membranes.add( ef );
+		membranes.add( fa );
+
+		final Junction G = junctionGraph.addVertex().init( 0, new double[] { 30, 0 } );
+		final Junction H = junctionGraph.addVertex().init( 0, new double[] { 40, 10 } );
+		final Junction I = junctionGraph.addVertex().init( 0, new double[] { 40, 20 } );
+		final Junction J = junctionGraph.addVertex().init( 0, new double[] { 30, 30 } );
+		junctions.add( G );
+		junctions.add( H );
+		junctions.add( I );
+		junctions.add( J );
+
+		final MembranePart bg = junctionGraph.addEdge( B, G ).init();
+		final MembranePart gh = junctionGraph.addEdge( G, H ).init();
+		final MembranePart hi = junctionGraph.addEdge( H, I ).init();
+		final MembranePart ij = junctionGraph.addEdge( I, J ).init();
+		final MembranePart jc = junctionGraph.addEdge( J, C ).init();
+		membranes.add( bg );
+		membranes.add( gh );
+		membranes.add( hi );
+		membranes.add( ij );
+		membranes.add( jc );
+
+		final Junction K = junctionGraph.addVertex().init( 0, new double[] { 50, 0 } );
+		final Junction L = junctionGraph.addVertex().init( 0, new double[] { 60, 10 } );
+		final Junction M = junctionGraph.addVertex().init( 0, new double[] { 60, 20 } );
+		final Junction N = junctionGraph.addVertex().init( 0, new double[] { 50, 30 } );
+		junctions.add( K );
+		junctions.add( L );
+		junctions.add( M );
+		junctions.add( N );
+
+		final MembranePart hk = junctionGraph.addEdge( H, K ).init();
+		final MembranePart kl = junctionGraph.addEdge( K, L ).init();
+		final MembranePart lm = junctionGraph.addEdge( L, M ).init();
+		final MembranePart mn = junctionGraph.addEdge( M, N ).init();
+		final MembranePart ni = junctionGraph.addEdge( N, I ).init();
+		membranes.add( hk );
+		membranes.add( kl );
+		membranes.add( lm );
+		membranes.add( mn );
+		membranes.add( ni );
+
+		final CellModel cellModel = FindFaces.findFaces( junctionModel );
+		return new GraphBundle( cellModel, junctionModel, junctions, membranes );
+	}
+
+	public static final void kickstart( final JunctionModel junctionModel, final CellModel cellModel )
+	{
+		final Context context = new Context();
+		final LeviathanWM wm = new LeviathanWM( context );
+		try
+		{
+			wm.setImagePath( LeviathanWM.dummyBdvData( junctionModel.getGraph() ) );
+		}
+		catch ( final SpimDataException e )
+		{
+			e.printStackTrace();
+		}
+		wm.setJunctionModel( junctionModel );
+		wm.setCellModel( cellModel );
+		new LeviathanMainWindow( wm ).setVisible( true );
+	}
+}
--- src/test/java/org/mastodon/leviathan/algorithms/CwIteratorExample.java
+++ src/test/java/org/mastodon/leviathan/algorithms/CwIteratorExample.java
@@ -1,76 +1,31 @@
 package org.mastodon.leviathan.algorithms;
 
-import org.mastodon.leviathan.algorithms.FaceIteratorGen.FaceIterator;
-import org.mastodon.leviathan.app.LeviathanJunctionAppModel;
-import org.mastodon.leviathan.app.LeviathanKeyConfigContexts;
+import org.mastodon.leviathan.TestUtils;
+import org.mastodon.leviathan.TestUtils.GraphBundle;
+import org.mastodon.leviathan.app.LeviathanWM;
 import org.mastodon.leviathan.model.junction.Junction;
-import org.mastodon.leviathan.model.junction.JunctionGraph;
 import org.mastodon.leviathan.model.junction.JunctionModel;
 import org.mastodon.leviathan.model.junction.MembranePart;
-import org.mastodon.leviathan.plugin.LeviathanPlugins;
-import org.mastodon.leviathan.views.bdv.LeviathanJunctionViewBdv;
-import org.mastodon.ui.coloring.feature.FeatureColorModeManager;
-import org.mastodon.ui.keymap.Keymap;
-import org.mastodon.ui.keymap.KeymapManager;
-import org.mastodon.util.DummySpimData;
-import org.mastodon.views.bdv.SharedBigDataViewerData;
-import org.scijava.ui.behaviour.KeyPressedManager;
-import org.scijava.ui.behaviour.util.Actions;
+import org.scijava.Context;
 
-import bdv.spimdata.SpimDataMinimal;
-import bdv.viewer.ViewerOptions;
+import mpicbg.spim.data.SpimDataException;
 
 public class CwIteratorExample
 {
 
-	public static void main( final String[] args )
+	public static void main( final String[] args ) throws SpimDataException
 	{
-		// Create example graph. 3 touching hexs.
-		final JunctionModel model = new JunctionModel( "pixel", "frame" );
-		final JunctionGraph graph = model.getGraph();
+		final GraphBundle bundle = TestUtils.getExampleGraph();
+		final JunctionModel model = bundle.junctionModel;
+		final MembranePart bc = bundle.membranes.get( 1 );
 
-		final Junction A = graph.addVertex().init( 0, new double[] { 10, 0 } );
-		final Junction B = graph.addVertex().init( 0, new double[] { 20, 10 } );
-		final Junction C = graph.addVertex().init( 0, new double[] { 20, 20 } );
-		final Junction D = graph.addVertex().init( 0, new double[] { 10, 30 } );
-		final Junction E = graph.addVertex().init( 0, new double[] { 0, 20 } );
-		final Junction F = graph.addVertex().init( 0, new double[] { 0, 10 } );
+		final LeviathanWM wm = new LeviathanWM( new Context() );
+		wm.setJunctionModel( model );
+		wm.setImagePath( LeviathanWM.dummyBdvData( model.getGraph() ) );
 
-		final MembranePart ab = graph.addEdge( A, B ).init();
-		final MembranePart bc = graph.addEdge( B, C ).init();
-		final MembranePart cd = graph.addEdge( C, D ).init();
-		final MembranePart de = graph.addEdge( D, E ).init();
-		final MembranePart ef = graph.addEdge( E, F ).init();
-		final MembranePart fa = graph.addEdge( F, A ).init();
-
-		final Junction G = graph.addVertex().init( 0, new double[] { 30, 0 } );
-		final Junction H = graph.addVertex().init( 0, new double[] { 40, 10 } );
-		final Junction I = graph.addVertex().init( 0, new double[] { 40, 20 } );
-		final Junction J = graph.addVertex().init( 0, new double[] { 30, 30 } );
-
-		final MembranePart bg = graph.addEdge( B, G ).init();
-		final MembranePart gh = graph.addEdge( G, H ).init();
-		final MembranePart hi = graph.addEdge( H, I ).init();
-		final MembranePart ij = graph.addEdge( I, J ).init();
-		final MembranePart jc = graph.addEdge( J, C ).init();
-
-		final Junction K = graph.addVertex().init( 0, new double[] { 50, 0 } );
-		final Junction L = graph.addVertex().init( 0, new double[] { 60, 10 } );
-		final Junction M = graph.addVertex().init( 0, new double[] { 60, 20 } );
-		final Junction N = graph.addVertex().init( 0, new double[] { 50, 30 } );
-
-		final MembranePart hk = graph.addEdge( H, K ).init();
-		final MembranePart kl = graph.addEdge( K, L ).init();
-		final MembranePart lm = graph.addEdge( L, M ).init();
-		final MembranePart mn = graph.addEdge( M, N ).init();
-		final MembranePart ni = graph.addEdge( N, I ).init();
-
-		final LeviathanJunctionAppModel appModel = toAppModel( model );
-		new LeviathanJunctionViewBdv( appModel );
-
-		final FaceIteratorGen itgen = new FaceIteratorGen( model.getGraph() );
+		final FaceIteratorGen< Junction, MembranePart > itgen = new FaceIteratorGen<>( model.getGraph() );
 		System.out.println( "Clockwise:" );
-		FaceIterator it = itgen.iterateCW( bc );
+		FaceIteratorGen< Junction, MembranePart >.FaceIterator it = itgen.iterateCW( bc );
 		while ( it.hasNext() )
 		{
 			final MembranePart next = it.next();
@@ -84,41 +39,6 @@ public class CwIteratorExample
 			System.out.println( next + " is CW ? " + it.isCW() );
 		}
 
+		TestUtils.kickstart( model, null );
 	}
-
-	public static final LeviathanJunctionAppModel toAppModel( final JunctionModel model )
-	{
-
-		final KeymapManager keymapManager = new KeymapManager();
-		final Keymap keymap = keymapManager.getForwardDefaultKeymap();
-		final KeyPressedManager keyPressedManager = new KeyPressedManager();
-		final ViewerOptions options = ViewerOptions.options()
-				.shareKeyPressedEvents( keyPressedManager )
-				.width( 400 )
-				.height( 300 );
-
-		final FeatureColorModeManager featureColorModeManager = new FeatureColorModeManager();
-		final LeviathanPlugins plugins = new LeviathanPlugins( keymap );
-		final Actions globalAppActions = new Actions( keymap.getConfig(), LeviathanKeyConfigContexts.LEVIATHAN );
-
-		final String path = "x=100 y=100 z=1 sx=1 sy=1 sz=1 t=10.dummy";
-		final SpimDataMinimal spimData = DummySpimData.tryCreate( path );
-		final SharedBigDataViewerData sharedBdvData = new SharedBigDataViewerData(
-				path,
-				spimData,
-				options,
-				() -> System.out.println( "Repaint" ) );
-
-		final LeviathanJunctionAppModel appModel =
-				new LeviathanJunctionAppModel(
-						model,
-						sharedBdvData,
-						keyPressedManager,
-						featureColorModeManager,
-						keymapManager,
-						plugins,
-						globalAppActions );
-		return appModel;
-	}
-
 }
