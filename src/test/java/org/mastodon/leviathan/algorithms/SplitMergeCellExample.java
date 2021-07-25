32680634f6484f8837676831414ed3e664b1578c
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2021-07-25 10:53:42 +0200

    In the split/merge example, split one cell programmatically.

--- src/test/java/org/mastodon/leviathan/algorithms/SplitMergeCellExample.java
+++ src/test/java/org/mastodon/leviathan/algorithms/SplitMergeCellExample.java
@@ -5,7 +5,9 @@ import org.mastodon.leviathan.TestUtils.GraphBundle;
 import org.mastodon.leviathan.app.LeviathanMainWindow;
 import org.mastodon.leviathan.app.LeviathanWM;
 import org.mastodon.leviathan.model.cell.CellModel;
+import org.mastodon.leviathan.model.junction.Junction;
 import org.mastodon.leviathan.model.junction.JunctionModel;
+import org.mastodon.leviathan.model.junction.MembranePart;
 import org.scijava.Context;
 
 import mpicbg.spim.data.SpimDataException;
@@ -17,6 +19,14 @@ public class SplitMergeCellExample
 		final GraphBundle bundle = TestUtils.getExampleGraph();
 		final JunctionModel junctionModel = bundle.junctionModel;
 		final CellModel cellModel = bundle.cellModel;
+
+		final FindFaces faceFinder = FindFaces.create( junctionModel.getGraph(), cellModel.getGraph() );
+		final MembranePart eref = junctionModel.getGraph().edgeRef();
+
+		// Link A to D.
+		final Junction vA = bundle.junctions.get( 0 );
+		final Junction vD = bundle.junctions.get( 3 );
+		faceFinder.split( vA, vD, eref );
 
 		final LeviathanWM wm = new LeviathanWM( new Context() );
 		wm.setImagePath( LeviathanWM.dummyBdvData( junctionModel.getGraph() ) );
